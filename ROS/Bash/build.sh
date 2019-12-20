a6b7bbcb257cff17343d96375c23be41ceee5d8b
BhavyanshM <bhavyanshmishra@gmail.com>
2019-12-20 03:47:30 -0600

    Remove ROS files

--- ROS/Bash/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-g++ -g -c -O2 -fPIC -o plugin.o main.cpp
-g++ -shared -o so-plugin.so plugin.o
-objcopy --only-keep-debug so-plugin.so so-plugin.debug
-strip --strip-debug so-plugin.so
