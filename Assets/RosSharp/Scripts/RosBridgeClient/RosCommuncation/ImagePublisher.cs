7a94427d3334ec56cb14ee524f706793945bafdf
BhavyanshM <bhavyanshmishra@gmail.com>
2020-01-01 20:54:07 -0600

    Add the following changes

--- Assets/RosSharp/Scripts/RosBridgeClient/RosCommuncation/ImagePublisher.cs
+++ Assets/RosSharp/Scripts/RosBridgeClient/RosCommuncation/ImagePublisher.cs
@@ -17,6 +17,8 @@ limitations under the License.
 // © Siemens AG, 2018, Dr. Martin Bischoff (martin.bischoff@siemens.com)
 
 using UnityEngine;
+using UnityEngine.Rendering;
+using Unity.Collections;
 
 namespace RosSharp.RosBridgeClient
 {
@@ -24,17 +26,22 @@ namespace RosSharp.RosBridgeClient
     {
         public Camera ImageCamera;
         public string FrameId = "Camera";
-        public int resolutionWidth = 640;
-        public int resolutionHeight = 480;
+        public int resolutionWidth = 1024;
+        public int resolutionHeight = 768;
         [Range(0, 100)]
-        public int qualityLevel = 50;
+        public int qualityLevel = 80;
+
+        public Material dmat;
 
         private MessageTypes.Sensor.CompressedImage message;
         private Texture2D texture2D;
         private Rect rect;
+        private RenderTexture rTex;
 
         protected override void Start()
         {
+            // ImageCamera.pixelRect = new Rect(0,0,resolutionWidth,resolutionHeight);
+            ImageCamera.depthTextureMode = DepthTextureMode.Depth;
             base.Start();
             InitializeGameObject();
             InitializeMessage();
@@ -52,21 +59,34 @@ namespace RosSharp.RosBridgeClient
             texture2D = new Texture2D(resolutionWidth, resolutionHeight, TextureFormat.RGB24, false);
             rect = new Rect(0, 0, resolutionWidth, resolutionHeight);
             ImageCamera.targetTexture = new RenderTexture(resolutionWidth, resolutionHeight, 24);
+            rTex = new RenderTexture(resolutionWidth, resolutionHeight, 24);
         }
 
         private void InitializeMessage()
         {
             message = new MessageTypes.Sensor.CompressedImage();
             message.header.frame_id = FrameId;
-            message.format = "jpeg";
+            message.format = "png";
         }
 
         private void UpdateMessage()
         {
+            // Debug.Log(ImageCamera.pixelWidth);
+            var oldRT = RenderTexture.active;
+            Graphics.Blit(ImageCamera.targetTexture, rTex, dmat);
             message.header.Update();
+
+            RenderTexture.active = rTex;
             texture2D.ReadPixels(rect, 0, 0);
-            message.data = texture2D.EncodeToJPG(qualityLevel);
+            texture2D.Apply();
+            message.data = ImageConversion.EncodeToPNG(texture2D);
             Publish(message);
+
+            RenderTexture.active = oldRT; 
+            // }
+            // Debug.Log("Published");
+
+
         }
 
     }
--- Assets/Scripts/DepthCamera.cs
+++ Assets/Scripts/DepthCamera.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System.IO; 
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,8 @@ using UnityEngine;
 public class DepthCamera : MonoBehaviour
 {
 	public Material mat;
+	public bool saved = false;
+	public Texture2D depthTexture;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,26 @@ public class DepthCamera : MonoBehaviour
     // Update is called once per frame
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        Graphics.Blit(source, destination, mat);
+    	Graphics.Blit(source, destination, mat); 
+        // Debug.Log("RT_Width:" + rentex.width + "\tRT_Height:" + rentex.height);
+        // if(saved == false){
+        	// Debug.Log("SAVED_PNG_Texture2D");
+        	// RenderTexture rentex = new RenderTexture(Camera.main.pixelWidth, Camera.main.pixelHeight, 24);
+        	// Graphics.Blit(source, rentex, mat); 
+        	// depthTexture = toTexture2D(rentex); 
+        	// saved = true;
+        	// byte[] bytes = depthTexture.EncodeToPNG();
+        	// File.WriteAllBytes(Application.dataPath + "/my_file.png", bytes);
+        // }
+        // Debug.Log("Width:" + Camera.main.pixelWidth + "\tHeight:" + Camera.main.pixelHeight); 
     }
+
+    Texture2D toTexture2D(RenderTexture rTex){
+
+	    Texture2D tex = new Texture2D(rTex.width, rTex.height, TextureFormat.RGB24, false);
+	    RenderTexture.active = rTex;
+	    tex.ReadPixels(new Rect(0, 0, rTex.width, rTex.height), 0, 0);
+	    tex.Apply();
+	    return tex;
+	}
 }
